1d8db0a8ba5840d92d7b58d51d3623001c29a6fb
Mugacha Gichamba <mugachadjango@gmail.com>
2019-02-22 12:40:12 +0300

    created calculateScore function to perform grading

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+
+
+
+var calculate_score = function(){
+  counter = "1";
+  var question1 = $("input:radio[name=question" + counter +"]:checked").val();
+  console.log(question1);
+}
+calculate_score();
