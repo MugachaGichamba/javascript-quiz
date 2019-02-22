9d94b327cc02608a5e58ce933a531c5e6b43b7cb
Mugacha Gichamba <mugachadjango@gmail.com>
2019-02-22 14:17:55 +0300

    Added a function to calculate percentage score

--- js/scripts.js
+++ js/scripts.js
@@ -6,5 +6,19 @@ $(document).ready(function(){
         total += parseInt($("input:radio[name=question" + count +"]:checked").val());
       }
   $("h1").before("<h2>Your score is: " +total + "<h2> ");
+  console.log(calculatePercentage(total));
   });
 });
+
+function calculatePercentage(total){
+  var percentageScore =  (total /10) * 100;
+  console.log(percentageScore);
+  if (percentageScore > 80){
+    return "You have excellently passed";
+} else if (percentageScore >= 50 && percentageScore > 80){
+return "You have fairly passed";
+}
+else {
+  return "You need to retake the test";
+}
+}
