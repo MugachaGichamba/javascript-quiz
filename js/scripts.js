13063852c91b39ff5a0f2b8bfc4e42f935d9827b
Mugacha Gichamba <mugachadjango@gmail.com>
2019-02-22 13:12:44 +0300

    Added click functionality to submit button

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,15 @@
-
-
+$(document).ready(function(){
+  $("button#submit").click(function(){
+    calculate_score();
+  });
+});
 
 var calculate_score = function(){
   total = 0
   for(count = 1; count <= 5; count++){
     count = parseInt(count);
     total += parseInt($("input:radio[name=question" + count +"]:checked").val());
-    console.log(total);
+
   }
   console.log(total);
 }
-calculate_score();
