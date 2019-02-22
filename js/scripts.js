d442f7b9bbdb9faf2329dbcb3a21e9367fc1ca68
Mugacha Gichamba <mugachadjango@gmail.com>
2019-02-22 13:33:49 +0300

    Added functionality to display score at the top of the page usung the .before() method

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,10 @@
 $(document).ready(function(){
   $("button#submit").click(function(){
-    calculate_score();
+      total = 0
+      for(count = 1; count <= 5; count++){
+        count = parseInt(count);
+        total += parseInt($("input:radio[name=question" + count +"]:checked").val());
+      }
+  $("h1").before("<h2>Your score is: " +total + "<h2> ");
   });
 });
-
-var calculate_score = function(){
-  total = 0
-  for(count = 1; count <= 5; count++){
-    count = parseInt(count);
-    total += parseInt($("input:radio[name=question" + count +"]:checked").val());
-
-  }
-  console.log(total);
-}
